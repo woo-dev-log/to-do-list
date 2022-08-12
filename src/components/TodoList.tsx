c1a80870c66f886e5ce220d4029abb5096472814
woo <>
2022-08-13 02:11:39 +0900

    기능 추가 및 빌드

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,12 +5,24 @@ import './TodoList.scss';
 
 const TodoList = () => {
     const todos = useRecoilValue(todoList);
+    const today = new Date();
+    const dateString = today.toLocaleString('ko-KR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+    const dayName = today.toLocaleString('ko-KR', { weekday: 'long' });
 
     return (
-        <div className='todo-list-container'>
+        <div className="todo-list-container">
+            <div className="todo-list-head">
+                <div className="todo-list-date">{dateString} {dayName}</div>
+                <span className="todo-list-length">해야 할 일: {todos.length}개</span>
+            </div>
+
             {todos.map((v) =>
                 <div key={v.id} className="todo-list">
-                    {v.contents}
+                    <span className="todo-list-contents">{v.contents}</span>
                     <TodoRemove id={v.id} />
                 </div>
             )}
